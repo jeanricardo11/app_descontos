6f838d5f97ab47d0763f93efe22d6457b238a21c
jeanricardo11 <72846631+jeanricardo11@users.noreply.github.com>
2026-09-25 20:32:45 -0300

    Refactor discount calculation and improve output messages

--- app_descontos.py
+++ app_descontos.py
@@ -1,29 +1,32 @@
-"""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""
              ***  APP DESCONTO ****
 Sistema de Descontos Progressivos para Loja Online
-"""""""""""""""""""""""""""""""""""""""""""""""""""""
-#Apresenta o App ao Usuario
-print ("Bem Vindo ao Sistema de Descontos !")
+"""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Apresenta o App ao Usuario
+print("Bem Vindo ao Sistema de Descontos !")
+
 # Solicita o valor da compra ao usuário
-valor_compra = float(input("Digite o valor da compra(R$):"))
+valor_compra = float(input("Digite o valor da compra (R$): "))
 
 # Define o desconto baseado no valor da compra
-if valor_compra < 200:
-    desconto_percentual = 5
-elif valor_compra < 300:
-    desconto_percentual = 10
-else:
-    desconto_percentual = 15
+match valor_compra:
+    case v if v < 200:
+        desconto_percentual = 5
+    case v if v < 300:
+        desconto_percentual = 10
+    case _:
+        desconto_percentual = 15
 
 # Calcula o valor do desconto
 valor_desconto = valor_compra * (desconto_percentual / 100)
 
-# Calcula o valor final a pagar
+# Calcula o valor final a pagar500
 valor_final = valor_compra - valor_desconto
 
 # Exibe os resultados
 
-print("Você recebeu")
-print(f"Desconto ({desconto_percentual}%): R$ {valor_desconto:.2f} de desconto")
-print(f"Com esse desconto sua compra ficará nop valor de: R$ {valor_final:.2f} !")
+print(f"Você recebeu um desconto de {desconto_percentual}%, no valor de: R$ {valor_desconto:.2f} ")
+print(f"Com esse desconto sua compra ficará no valor de: R$ {valor_final:.2f} !")
+
 
